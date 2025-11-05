28c367251b67a4cc69f865c6581d71d092e6024d
hrisimov <140465371+hrisimov@users.noreply.github.com>
2025-11-05 23:11:12 +0200

    Added `HomePage` component as a child route

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,19 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import HomePage from '@/pages/HomePage';
+
 import App from './App';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+    ],
   },
 ]);
 
