e2f3ae8f502f0654ac843dee6bee7f1a7a85fd16
hrisimov <140465371+hrisimov@users.noreply.github.com>
2025-12-17 13:56:22 +0200

    Refactored `HomePage` component with `useFetch`

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,52 +1,22 @@
-import axios from 'axios';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 
-import api from '@/api';
 import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
 import { Separator, Spinner } from '@/components/ui';
+import useFetch from '@/hooks/useFetch';
 
 const HomePage = () => {
-  const [listings, setListings] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
   const [filters, setFilters] = useState({
     dates: undefined,
     guests: 0,
     search: '',
   });
 
-  const abortController = useRef(null);
-
-  useEffect(() => {
-    const fetchListings = async () => {
-      setIsLoading(true);
-      setError(null);
-
-      abortController.current = new AbortController();
-
-      try {
-        const response = await api.get('/api/listings', {
-          params: filters,
-          signal: abortController.current?.signal,
-        });
-        setListings(response.data);
-      } catch (error) {
-        if (axios.isCancel(error)) {
-          return;
-        }
-        setError('Something went wrong. Please try again later.');
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchListings();
-
-    return () => {
-      abortController.current?.abort();
-    };
-  }, [filters]);
+  const {
+    data: listings,
+    error,
+    isLoading,
+  } = useFetch('/api/listings', { params: filters });
 
   const handleFilters = (filters) => {
     setFilters(filters);
