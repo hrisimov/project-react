66ac4e6e9b79f17d55f9d7b43958dd3c0af8b159
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-02-04 17:15:11 +0200

    Created the `fetchListings` extra reducers

--- src/state/listings/listingsSlice.js
+++ src/state/listings/listingsSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 import api from '@/api';
 
@@ -12,6 +13,24 @@ const listingsSlice = createSlice({
   name: 'listings',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchListings.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchListings.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.listings = action.payload;
+      })
+      .addCase(fetchListings.rejected, (state, action) => {
+        if (axios.isCancel(action.payload)) {
+          return;
+        }
+
+        state.status = 'failed';
+        state.error = action.payload.message;
+      });
+  },
 });
 
 export const fetchListings = createAsyncThunk(
