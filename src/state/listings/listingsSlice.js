2be239e77dcaee12a6b3e0b03d65f53df9e3ccdf
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-02-05 03:21:50 +0200

    Setted up favorite listings and edited rejected state in `extraReducers`

--- src/state/listings/listingsSlice.js
+++ src/state/listings/listingsSlice.js
@@ -1,18 +1,27 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
 
 import api from '@/api';
 
 const initialState = {
   listings: [],
   error: null,
+  favoriteListingIds: [],
   status: 'idle',
 };
 
 const listingsSlice = createSlice({
   name: 'listings',
   initialState,
-  reducers: {},
+  reducers: {
+    addFavoriteListing: (state, action) => {
+      state.favoriteListingIds.push(action.payload);
+    },
+    removeFavoriteListing: (state, action) => {
+      state.favoriteListingIds = state.favoriteListingIds.filter(
+        (id) => id !== action.payload,
+      );
+    },
+  },
   extraReducers(builder) {
     builder
       .addCase(fetchListings.pending, (state) => {
@@ -23,12 +32,12 @@ const listingsSlice = createSlice({
         state.listings = action.payload;
       })
       .addCase(fetchListings.rejected, (state, action) => {
-        if (axios.isCancel(action.payload)) {
-          return;
-        }
+        const message = action.error.message;
 
-        state.status = 'failed';
-        state.error = action.payload.message;
+        if (message !== 'Aborted') {
+          state.status = 'failed';
+          state.error = message;
+        }
       });
   },
 });
@@ -40,5 +49,8 @@ export const fetchListings = createAsyncThunk(
     return response.data;
   },
 );
+
+export const { addFavoriteListing, removeFavoriteListing } =
+  listingsSlice.actions;
 
 export default listingsSlice.reducer;
