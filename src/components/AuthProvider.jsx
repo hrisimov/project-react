bce42e834375321f582c91f51ca445a1a6f52abf
hrisimov <140465371+hrisimov@users.noreply.github.com>
2026-02-22 00:30:28 +0200

    Added functionality for refreshing the `token` when expired

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -38,14 +38,48 @@ const AuthProvider = ({ children }) => {
 
   useLayoutEffect(() => {
     const authInterceptor = api.interceptors.request.use((config) => {
-      config.headers.Authorization = token
-        ? `Bearer ${token}`
-        : config.headers.Authorization;
+      config.headers.Authorization =
+        !config._retry && token
+          ? `Bearer ${token}`
+          : config.headers.Authorization;
       return config;
     });
 
     return () => {
       api.interceptors.request.eject(authInterceptor);
+    };
+  }, [token]);
+
+  useLayoutEffect(() => {
+    const refreshInterceptor = api.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config;
+
+        if (
+          error.response.status === 403 &&
+          error.response.data.message === 'Unauthorized'
+        ) {
+          try {
+            const response = await api.get('/api/refreshToken');
+
+            setToken(response.data.accessToken);
+
+            originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
+            originalRequest._retry = true;
+
+            return api(originalRequest);
+          } catch {
+            setToken(null);
+          }
+        }
+
+        return Promise.reject(error);
+      },
+    );
+
+    return () => {
+      api.interceptors.response.eject(refreshInterceptor);
     };
   }, [token]);
 
